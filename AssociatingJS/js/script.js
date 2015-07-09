5410aedb70cfa7f1522f151b1eba3335f49d59f5
benluls <blangell@fullsail.edu>
2015-07-09 18:54:19 -0500

    Defining Variables
    defining varibales

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,3 +1,7 @@
- // JavaScript
+ // javascript
 
- var yearBorn; 
+ var age; //declaration
+ age = 24; //definition
+ //storage container = value
+
+ car yearOfBirth = 1991; //declaring and defining on the same line
