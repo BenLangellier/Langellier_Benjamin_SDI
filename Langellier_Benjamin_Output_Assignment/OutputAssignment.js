fefdf7c9a1eaa00583ae700a32ed70c1042349a5
benluls <blangell@fullsail.edu>
2015-07-09 20:36:51 -0500

    Langellier_Benjamin_Output_Assignment
    Commit 5

--- Langellier_Benjamin_Output_Assignment/OutputAssignment.js
+++ Langellier_Benjamin_Output_Assignment/OutputAssignment.js
@@ -1,3 +1,4 @@
+// Benjamin Langellier, SDI section 2, Output Assignment
 
 var myJob = 'Warehouse Manager';
 var favoriteNumber = 19;
@@ -7,6 +8,7 @@ alert("I am currently employed at Nord Commercial Services, what do I do?");
 alert(" my favorite number comes from the number I was given when I was a kid playing travel soccer, what is it?");
 alert(" I like living in Illinois but would I like to move?");
 alert("My answers to each question are located inside of the console")
+
 console.log('What is my position?');
 console.log(myJob);
 console.log("What is my favorite number?");
