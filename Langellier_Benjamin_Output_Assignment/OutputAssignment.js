86af38af0f4f82e8fe7b236706f666c6691cc288
benluls <blangell@fullsail.edu>
2015-07-09 20:34:16 -0500

    Output commit 4
    commit 4

--- Langellier_Benjamin_Output_Assignment/OutputAssignment.js
+++ Langellier_Benjamin_Output_Assignment/OutputAssignment.js
@@ -7,7 +7,9 @@ alert("I am currently employed at Nord Commercial Services, what do I do?");
 alert(" my favorite number comes from the number I was given when I was a kid playing travel soccer, what is it?");
 alert(" I like living in Illinois but would I like to move?");
 alert("My answers to each question are located inside of the console")
-
+console.log('What is my position?');
 console.log(myJob);
+console.log("What is my favorite number?");
 console.log(19);
+console.log('Do I want to move from Illinois?');
 console.log(desireToMove);
