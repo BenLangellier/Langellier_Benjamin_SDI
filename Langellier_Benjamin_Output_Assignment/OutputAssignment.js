1328ebc22085c763282b9e57729995a9359e50ab
benluls <blangell@fullsail.edu>
2015-07-09 20:20:37 -0500

    Output Assignment
    Benjamin Langellier’s
    Output Assignment

---
+++ Langellier_Benjamin_Output_Assignment/OutputAssignment.js
@@ -0,0 +1,11 @@
+
+var myJob = 'Warehouse Manager';
+var favoriteNumber = 19;
+var desireToMove = true;
+
+alert("I am currently employed at Nord Commercial Services, what do I do?");
+alert(" my favorite number comes from the number I was given when I was a kid playing travel soccer, what is it?");
+alert(" I like living in Illinois but would I like to move?");
+alert("My answers to each question are located inside of the console")
+
+console.log()
