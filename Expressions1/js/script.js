d45ae2bb25ceb503889584816b93cfda56426209
benluls <blangell@fullsail.edu>
2015-07-10 23:42:42 -0500

    Assignment Operators
    Assignment Operators

--- Expressions1/js/script.js
+++ Expressions1/js/script.js
@@ -2,9 +2,7 @@
  * Created by BenLangellier on 7/10/15.
  */
 
-var areaCode = 407;
-var firstPart = 203;
-var secPart = 4529;
-var phoneNo = "(" + String(areaCode) + ")" +  String(firstPart) + "-" + String(secPart);
-//(407) 203-4529
-console.log(phoneNo);
+var a = 3;
+//a ++; // a = a + 1
+a --;
+console.log(a);
