bce950d803403032dfd512460d71b27e8022ca35
benluls <blangell@fullsail.edu>
2015-07-13 22:07:49 -0500

    Changes made
    fixed html and tweaked comments

--- Expressions_worksheet/js/Slice_of_Pie2.js
+++ Expressions_worksheet/js/Slice_of_Pie2.js
@@ -14,8 +14,8 @@ var result; //calculation of how many pieces of pizza each person will get
 totalPizzas = prompt("How many pizzas were ordered to the party?:"); //user puts in how many pizzas were ordered
 slicesPizza = prompt("How many slices are there per pizza?:"); //user puts in how many slices each pizza has
 people      = prompt("How many people showed up to this awesome party?"); //user tells how many people were at the party
-result = totalPizzas * slicesPizza % people; //final calculation for the amount of pizza each person will get
+result = totalPizzas * slicesPizza % people; //final calculation for the amount of pizza sparky will get I added the % modulo to find the given number of slices Sparky will get
 
 //Output
 
-console.log("Each person ate " + result + " " + "slices of pizza at the party"); //output to show user how much pizza each person will get
+console.log("Sparky got  " + result + " " + "slices of pizza"); //output to show user how much pizza sparky will get
