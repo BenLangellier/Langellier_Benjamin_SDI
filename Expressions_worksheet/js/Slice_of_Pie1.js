36a1631f2d08e1642f37cf4020ceb4298c80ffb8
benluls <blangell@fullsail.edu>
2015-07-13 21:52:37 -0500

    Final
    Final

--- Expressions_worksheet/js/Slice_of_Pie1.js
+++ Expressions_worksheet/js/Slice_of_Pie1.js
@@ -4,18 +4,18 @@
 
 //variables
 
-var slicesPizza;
-var people;
-var totalPizzas;
-var result;
+var slicesPizza; //declaring the variable for slices of pizza
+var people; // declaring the variable for amount of people
+var totalPizzas; //declaring the amount of pizza ordered to the party
+var result; //calculation of how many pieces of pizza each person will get
 
 //prompts
 
-totalPizzas = prompt("How many pizzas were ordered to the party?:");
-slicesPizza = prompt("How many slices are there per pizza?:");
-people      = prompt("How many people showed up to this awesome party?");
-result = totalPizzas * slicesPizza / people;
+totalPizzas = prompt("How many pizzas were ordered to the party?:"); //user puts in how many pizzas were ordered
+slicesPizza = prompt("How many slices are there per pizza?:"); //user puts in how many slices each pizza has
+people      = prompt("How many people showed up to this awesome party?"); //user tells how many people were at the party
+result = totalPizzas * slicesPizza / people; //final calculation for the amount of pizza each person will get
 
 //Output
 
-console.log("Each person ate " + result + " " + "slices of pizza at the party");
+console.log("Each person ate " + result + " " + "slices of pizza at the party"); //output to show user how much pizza each person will get
