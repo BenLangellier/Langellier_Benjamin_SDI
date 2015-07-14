2d12e305e0fbb27b8420513161e19cac99d4f7cb
benluls <blangell@fullsail.edu>
2015-07-13 21:43:43 -0500

    Vars and prompts

--- Expressions_worksheet/js/Slice_of_Pie1.js
+++ Expressions_worksheet/js/Slice_of_Pie1.js
@@ -1,3 +1,21 @@
 //Benjamin Langellier
 //SDI 1507 Section 2
-//
+//Expression Worksheet
+
+//variables
+
+var slicesPizza;
+var people;
+var totalPizzas;
+var result;
+
+//prompts
+
+totalPizzas = prompt("How many pizzas were ordered to the party?:");
+slicesPizza = prompt("How many slices are there per pizza?:");
+people      = prompt("How many people showed up to this awesome party?");
+result = totalPizzas * slicesPizza / people;
+
+//Output
+
+
