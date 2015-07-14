e11f540bd4ff403318ae18cbb8f578aad80442ae
benluls <blangell@fullsail.edu>
2015-07-13 21:24:25 -0500

    Comments added
    Final w/ comments

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -6,13 +6,14 @@
 
 var sparkysAge; //declare variable for human years age
 var sparkysDogYears; //declare variable for dog years
-var result;
+var result; // declare variable for final result
 
 //input and calculation of input
 
-sparkysAge = prompt("How old is Sparky?:");
-sparkysDogYears = alert("Dog years are seven times faster than human years \nwhich means Human years * 7 = Sparkys age");
-result = sparkysAge * 7;
+sparkysAge = prompt("How old is Sparky?:"); //user inputs age of sparky
+sparkysDogYears = alert("Dog years are seven times faster than human years \nwhich means Human years * 7 = Sparkys age"); //explanation of process of finding the dogs years
+result = sparkysAge * 7; //calculation of dog years
 //output
 
-console.log("Sparky is " + sparkysAge + " " + "human years old which is " + result + " " + "in dog years");
+console.log("Sparky is " + sparkysAge + " " + "human years old which is " + result + " " + "in dog years");//Final output
+
