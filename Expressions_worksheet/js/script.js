3e49c99908aa360cc84d7584b472df1f91d60bcc
benluls <blangell@fullsail.edu>
2015-07-13 19:32:49 -0500

    Expressions Worksheet
    Original

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,8 @@
+//Benjamin Langellier
+//SDI 1507 Section 2
+//Expressions worksheet
+
+var sparkysAge = prompt("We are trying to find out how old Sparky is. \n In human years how old is Sparky?");
+var sparkysDogYears = sparkysAge * 7;
+console.log(sparkysDogYears);
+alert("Sparkys age in dog years located in the console");
