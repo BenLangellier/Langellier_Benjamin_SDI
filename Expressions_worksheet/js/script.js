da0d1fb2e7e18b789999eefcf5c63cd7423606e4
benluls <blangell@fullsail.edu>
2015-07-13 19:42:25 -0500

    Sparky problem
    commit 2

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -2,7 +2,7 @@
 //SDI 1507 Section 2
 //Expressions worksheet
 
-var sparkysAge = prompt("We are trying to find out how old Sparky is. \n In human years how old is Sparky?");
-var sparkysDogYears = sparkysAge * 7;
-console.log(sparkysDogYears);
-alert("Sparkys age in dog years located in the console");
+var sparkysAge = prompt("We are trying to find out how old Sparky is. \n In human years how old is Sparky?"); //This is for the input of the dogs human years
+var sparkysDogYears = sparkysAge * 7; //This is how you calculate the dogs age in dog years
+console.log(sparkysDogYears); //putting the dogs age as the output
+alert("Sparkys age in dog years located in the console"); //letting the user know where the answer is located.
