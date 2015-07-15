11bafc9ec4ddbbabf2cd7c9663a8535779feb6dd
benluls <blangell@fullsail.edu>
2015-07-14 19:18:10 -0500

    Average Shopping bills
    beginning / forgot to commit until about half way through

--- Expressions_worksheet/js/Average_shopping_bill.js
+++ Expressions_worksheet/js/Average_shopping_bill.js
@@ -2,4 +2,14 @@
 //SDI 1507 Section 2
 //Expression Worksheet
 
-alert("Java Works!?")
+//variables
+
+var shoppingArray = [105, 107, 210, 303, 45]; //declaring shopping array
+
+//result variables
+
+var totalShopping = shoppingArray[0] + shoppingArray[1] + shoppingArray[2] + shoppingArray[3] + shoppingArray[4]; // calculating the total for the amount of my last 5 Shopping bills
+// making sure the totalShopping var worked console.log(totalShopping);
+var averageShopping = totalShopping/5;
+// making sure all the variables worked together correctly //console.log(averageShopping);
+console.log("Your last five weeks of shopping bills are " + shoppingArray); //begining to show the problem
