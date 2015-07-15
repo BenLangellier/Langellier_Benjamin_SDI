d9104eea6f78e2cf0b6eac138f1467cf1cf29766
benluls <blangell@fullsail.edu>
2015-07-15 16:02:28 -0500

    Expressions Assignment

--- Lastname_Firstname_Expressions_Assignment/js/java.js
+++ Lastname_Firstname_Expressions_Assignment/js/java.js
@@ -13,12 +13,18 @@ var overallHeight;
 var overallWeight;
 var lastHeight;
 var bMI;
+//prompts
+
 heightFeet = prompt("What is your height in feet?:");
 heightInches = prompt("What is your remainder of your height in inches?:");
 weight = prompt("What is your weight?:");
+
+//calculations
 overallHeight = (heightFeet*12)+4;
 overallWeight = weight*703;
 lastHeight = overallHeight*overallHeight;
 bMI = overallWeight/lastHeight;
+
+//output
 console.log("Your BMI is " + bMI);
 
