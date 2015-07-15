05cd81735bff51149d76c77e932a64a0a617c78c
benluls <blangell@fullsail.edu>
2015-07-14 19:28:53 -0500

    Discounts start
    starting naming the variables for the final problem.

---
+++ Expressions_worksheet/js/discount.js
@@ -0,0 +1,10 @@
+//Benjamin Langellier
+//SDI 1507 Section 2
+//Expression Worksheet
+
+// making sure java is connected correctly alert("Java works?");
+
+var originalPrice;
+var discountPercent;
+var item;
+var salesTax;
