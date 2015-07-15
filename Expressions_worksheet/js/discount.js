bca7b4039418781e054877a2f7ab8e7c050a4fed
benluls <blangell@fullsail.edu>
2015-07-14 19:42:22 -0500

    variables and initial comments put in

--- Expressions_worksheet/js/discount.js
+++ Expressions_worksheet/js/discount.js
@@ -3,8 +3,14 @@
 //Expression Worksheet
 
 // making sure java is connected correctly alert("Java works?");
+//variables
+var originalPrice;//declaring the original price variable
+var discountPercent;//declaring the discount percent
+var item;//declaring the variable for the item
+var salesTax;//declaring the sales tax
+item = prompt("What is the item that you are buying?:");//setting up the prompt for price
+originalPrice   = prompt("What is the original price of your item?:");//user settting the price of the item
+discountPercent = prompt("What is the discount percentage?:");//user setting the discount percent
+salesTax        = prompt("What is the sales tax percentage?;");//user setting the sales tax
 
-var originalPrice;
-var discountPercent;
-var item;
-var salesTax;
+//checking make sure prompts work
