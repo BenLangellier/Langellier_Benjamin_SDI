3a225280b8b9799e961e6116cddb23eaf27ce9cc
benluls <blangell@fullsail.edu>
2015-07-14 19:22:03 -0500

    Final
    Final w/ test

--- Expressions_worksheet/js/Average_shopping_bill.js
+++ Expressions_worksheet/js/Average_shopping_bill.js
@@ -12,4 +12,5 @@ var totalShopping = shoppingArray[0] + shoppingArray[1] + shoppingArray[2] + sho
 // making sure the totalShopping var worked console.log(totalShopping);
 var averageShopping = totalShopping/5;
 // making sure all the variables worked together correctly //console.log(averageShopping);
-console.log("Your last five weeks of shopping bills are " + shoppingArray); //begining to show the problem
+console.log("Your last five weeks of shopping bills are " + shoppingArray); // showing the prices used.
+console.log("You have spent a total of $" + totalShopping + " " + "on groceries over 5 weeks. That is an average of $" + averageShopping + " " + "per week."  ); // connecting all the varaibles into the final form
