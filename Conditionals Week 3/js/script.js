37d13eaae343d00065452ea62342c41603d93d0e
benluls <blangell@fullsail.edu>
2015-07-21 16:51:55 -0500

    w/ and Expression

--- Conditionals Week 3/js/script.js
+++ Conditionals Week 3/js/script.js
@@ -1,11 +1,11 @@
-//Conditional Logic - Relational Expressions
+//Conditional Logic - With an expression
 
-var kidHeight = 50;
+var kidHeight = 47;
 var minHeight = 48;
-
+var sneakerLift = 2;
 //if the child is old enough print to the console " you can ride!"
 //if the kid is over 48 inches in height
-if(kidHeight > minHeight){
+if(kidHeight + sneakerLift > minHeight){
     //code performed if condition is true
     console.log("You can ride the coaster!");
 
