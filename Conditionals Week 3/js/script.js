b5a3502d8701add4de72a8adee0feb22f0f1fee8
benluls <blangell@fullsail.edu>
2015-07-21 20:46:16 -0500

    Else if and Relational Operators

--- Conditionals Week 3/js/script.js
+++ Conditionals Week 3/js/script.js
@@ -1,15 +1,19 @@
-//Conditional Logic - With an expression
+//Conditional Logic - Relational Operators
 
-var kidHeight = 49;
+var kidHeight = 52;
 var minHeight = 48;
-
+var wParentHeight = 45; // the height of the kid with parent
 //if the child is old enough print to the console " you can ride!"
 //if the kid is over 48 inches in height
 if(kidHeight > minHeight){
-    //code performed if condition is true
+    //you can ride
+    // /code performed if condition is true
     console.log("You can ride the coaster!");
 
-}else{
+}else if(kidHeight > wParentHeight){
     //code performed if condition is false
+    console.log("You can ride, but only with a parent.");
+}else{
+    //sorry you have growing to do
     console.log("Sorry, you've got some growing to do first.");
 }
