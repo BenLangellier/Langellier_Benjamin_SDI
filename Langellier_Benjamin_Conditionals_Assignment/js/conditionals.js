44535342f72d0addb467517d1321c9125f9e8c63
benluls <blangell@fullsail.edu>
2015-07-23 16:08:20 -0500

    Re-made

--- Langellier_Benjamin_Conditionals_Assignment/js/conditionals.js
+++ Langellier_Benjamin_Conditionals_Assignment/js/conditionals.js
@@ -3,9 +3,26 @@
 //Conditionals Assignment
 
 //console.log("Hello World!"); //Testing connection to the HTML page - connection works
-//Will the user play volleyball or go golfing?
-// if it is sunny and 75 degrees out or greater than the user will go golfing
-//if it is not sunny or under 75 degrees the user will go play volleyball
+//Your Sibling has friends over and wants to take them to an R rated movie you are figuring out if they are able to do this
+//We will be recieving variables of the ages of him and his friends as well as yourself to figure out if they can go
 
-var temp = prompt("What is the temperature outside?");
-var weather = confirm("is it sunny outside? /n True is Okay, False is Cancel");
+var myAge;
+var siblingsAge;
+var legalAge = 18;
+var friends;
+var myAge = Number(prompt("How old are you?"));
+var siblingsAge = Number(prompt("How old is your sibling?"));
+//var friends = confirm("Are your siblings friends above the legal age? \nTrue is Okay, False is Cancel");
+
+
+
+if(siblingsAge >= legalAge){
+    console.log("Go see the movie!");
+}else if(myAge >= legalAge){
+    console.log("Let's go see the movie!")
+}else{
+    console.log("We need to grow up first...")
+}
+
+//(friends === true) ? console.log("Why are you asking me, just go to the movie.") : console.log("Yeah we need to grow up");
+//wanted to show that I knew how to make the Ternary statement
