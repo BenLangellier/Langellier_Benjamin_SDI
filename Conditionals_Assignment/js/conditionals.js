eab7ebad711bb23741e770274398fff58bd32f7a
benluls <blangell@fullsail.edu>
2015-07-22 19:40:17 -0500

    Figuring out variables

--- Conditionals_Assignment/js/conditionals.js
+++
@@ -1,5 +0,0 @@
-//Benjamin Langellier
-//SDI 1507 Section 2
-//Conditionals Assignment
-
-//console.log("Hello World!"); //Testing connection to the HTML page - connection works
---
+++ Langellier_Benjamin_Conditionals_Assignment/js/conditionals.js
@@ -0,0 +1,11 @@
+//Benjamin Langellier
+//SDI 1507 Section 2
+//Conditionals Assignment
+
+//console.log("Hello World!"); //Testing connection to the HTML page - connection works
+//Will the user play volleyball or go golfing?
+// if it is sunny and 75 degrees out or greater than the user will go golfing
+//if it is not sunny or under 75 degrees the user will go play volleyball
+
+var temp = prompt("What is the temperature outside?");
+var weather = confirm("is it sunny outside? /n True is Okay, False is Cancel");
